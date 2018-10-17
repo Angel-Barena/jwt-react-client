840b9135dc20b48b922ef77610a0f6df7898eaeb
angel-barena <angelbarena89@hotmail.com>
2018-10-17 12:08:48 -0500

    make it better

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import {AUTH_USER, AUTH_ERROR} from './types';
 
 export const signup = (formProps, callback) => async dispatch=>{
     try {
-        const response = await axios.post('http://localhost:3090/signup', formProps);
+        const response = await axios.post('https://authjwtserver.herokuapp.com/signup', formProps);
 
         dispatch({ type: AUTH_USER, payload: response.data.token});
         localStorage.setItem('token', response.data.token)
@@ -17,7 +17,7 @@ export const signup = (formProps, callback) => async dispatch=>{
 
 export const signin = (formProps, callback) => async dispatch=>{
     try {
-        const response = await axios.post('http://localhost:3090/signin', formProps);
+        const response = await axios.post('https://authjwtserver.herokuapp.com/signin', formProps);
 
         dispatch({ type: AUTH_USER, payload: response.data.token});
         localStorage.setItem('token', response.data.token)
